bf1978235662d58ce6c542790d7c8d833ed0da4f
inspectorfiby <phoebezcy93@gmail.com>
2017-10-25 15:59:03 -0400

    signup and signin implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
