4da1e3999537e6cba4600194c9a3a19f43ff8b58
inspectorfiby <phoebezcy93@gmail.com>
2017-10-26 15:36:00 -0400

    made changes to models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
   has_many :board_enrollments
   has_many :users, through: :board_enrollments
-  has_many :lists
+  has_many :lists, class_name: :List, foreign_key: :board_id
   has_one :leader, class_name: :User, foreign_key: :leader_id
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ApplicationRecord
   belongs_to :board, foreign_key: :board_id
-  has_many :cards
+  has_many :cards, class_name: :Card, foreign_key: :list_id
 end
