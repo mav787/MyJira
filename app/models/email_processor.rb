02105e93a679152e190a8391f71f609d4d90c3ca
inspectorfiby <phoebezcy93@gmail.com>
2017-12-08 13:46:37 -0500

    card mailer

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -12,13 +12,13 @@ class EmailProcessor
     done_list = List.where("board_id = ?", board.id).where("name = ?", "done").first
     doing_list = List.where("board_id = ?", board.id).where("name = ?", "doing").first
 
-    if (token_recipient_id != 0)
+    if (token_recipient_id != "0")
       comment = Comment.create(context: @email.body, from_user_id: author.id, card_id: token_task_id, to_user_id: token_recipient_id)
       n = Notification.create(recipient_id: token_recipient_id, comment_id: comment.id, card_id: token_task_id, read: false, source: "comment")
       CommentMailer.comment_note(n.recipient, n).deliver
     else
-      if @email.body.start_with? "done" && card.list.name == "doing"
-        card.list_id = doine_list.id
+      if @email.body.start_with?("done") && card.list.name == "doing"
+        card.list_id = done_list.id
         card.finished_at = Time.now
         card.card_order = 1
         card.save
@@ -32,7 +32,7 @@ class EmailProcessor
                                      list_id: done_list.id,
                                      order: 1
 
-      elsif @email.body.start_with? "doing"
+      elsif @email.body.start_with?("doing")
         card.list_id = doing_list.id
         card.startdate = Time.now
         card.card_order = 1
