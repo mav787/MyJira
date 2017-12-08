56f6de4fbd2141161ec8590ccb47d2faead5ddcc
inspectorfiby <phoebezcy93@gmail.com>
2017-12-07 21:39:38 -0500

    webhook test

--- app/controllers/github_webhook_controller.rb
+++ app/controllers/github_webhook_controller.rb
@@ -5,12 +5,23 @@ class GithubWebhooksController < ActionController::Base
     repo = params["repository"]["full_name"]
     board = Board.where("repo =? ", repo).first
     done_list = List.where("board_id = ?", board.id).where("name = ?", "done").first
+    doing_list = List.where("board_id = ?", board.id).where("name = ?", "doing").first
     params["commits"].each do |c|
-      board.lists.each do |l|
-        l.cards.each do |card|
-          if card.content == c["message"]
-            redirect_to '/card/move', :params =>{:card_id => card.id, :new_list_id => done_list.id}
+      doing_list.cards.each do |card|
+        if card.content == c["message"]
+          card.list_id = done_list
+          card.finished_at = Time.now
+          card.card_order = 1
+          card.save
+          new_list_cards = Card.where("list_id = ? AND card_order >= ?", done_list.id, 1).where.not(id:moving_card.id)
+          new_list_cards.each do |card|
+            card.card_order += 1
+            card.save
           end
+          ActionCable.server.broadcast "team_#{board.id}_channel",
+                                       card_id: card.id,
+                                       list_id: done_list.id,
+                                       order: 1
         end
       end
     end
