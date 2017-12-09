c477466636afd217dbeee4712d70896cca4784c9
inspectorfiby <phoebezcy93@gmail.com>
2017-12-09 16:59:12 -0500

    deleted unused files

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,6 +1,6 @@
 Griddler.configure do |config|
-  config.processor_class = EmailProcessor # CommentViaEmail
-  config.email_class = Griddler::Email # MyEmail
+  config.processor_class = EmailProcessor
+  config.email_class = Griddler::Email 
   config.processor_method = :process
   config.reply_delimiter = '-- REPLY ABOVE THIS LINE --'
   config.email_service = :postmark
